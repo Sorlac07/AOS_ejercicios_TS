3338966c29a902e423d9b2da956c2148d6bd4f6e
Charlie <u202120841@upc.edu.pe>
2023-09-06 18:27:36 -0500

    Ejercicio 4 -> solucion

--- app.ts
+++ app.ts
@@ -1,18 +1,11 @@
-// 3.	Construir un programa TS que permita ingresar N números enteros a un vector, 
-//      luego determine el primer número par de las posiciones pares
+ // 4.	Guardar 5 números en un vector tal como A y 9 números en otro vector tal como B,
+ //  luego trasladar el último número de A al final de B.
 
-let tam: any = prompt("Ingrese el tamaño del vector: ");
-let num1: number = parseInt(tam);
-let vector: number[] = new Array();
-for (let i=0; i<num1; i++ ){
-    let num2: any = prompt("Ingrese el el numero: "); 
-    vector.push(parseInt(num2))
-}
+ let A: number[] = new Array(3, 5, 7, 9, 11);
+ let B: number[] = new Array(53, 23, 12, 65, 34, 76, 98, 45, 67);
+//before 
+prompt("El vector A es: " + A + "\nEl vector B es: " + B);
 
-for (let i=1; i<num1; i = i+2 ){
-    console.log(i);
-    if (vector[i]%2==0){
-        prompt("El primer numero par de las posiciones pares es: " + vector[i]);
-        break;
-    }
-}
+B.push(A.pop()!); // ! = asegura que no sea null
+// after
+prompt("El vector A es: " + A + "\nEl vector B es: " + B);
