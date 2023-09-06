281ba72f4ec6878d71adeb3434eb645c3b688d33
Charlie <u202120841@upc.edu.pe>
2023-09-06 14:08:28 -0500

    Ejercicio 2 -> solucion

--- app.ts
+++ app.ts
@@ -1,11 +1,11 @@
-//1.	Diseñar un programa TS que permita calcular la suma de los primeros N términos de la siguiente serie: 
-//   	1  - 1/3 +	1/5	-	1/7	+	1/9	-	…
+// 2.	Diseñar un programa TS que permita calcular la suma de los primeros N términos de la siguiente serie: 
+// 64/21		+	62/24		+	60/27		+	58/30		+	…
 
 let number: any = prompt("Ingrese el numero de terminos a sumar: ");
 let number1: number = parseInt(number);
 let suma: number = 0;
 
-for (let i = 0; i < number; i++) {
-    suma += (-1) ** (i) / (1 + 2 * i);
+for (let i = 0; i < number1; i++) {
+    suma = + suma + (64 - (i * 2)) / (21 + (i * 3));
 }
 prompt("La suma es",suma.toString());
